b2dbd7f7cca1db8dc58d95a03df3771377a37602
javzero <Javzero1@gmail.com>
2018-12-29 18:49:32 -0300

    Adding crontest | Testing schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('carts:purge')->cron('* * * * *');
+        $schedule->command('cron:test')->everyMinute();
     }
 
     /**
